c8d43204b346f4d46061714bfdb90218d9259d08
Przemek89k <przemekgronski@gmail.com>
2023-02-18 14:36:22 +0100

    Constraints without quotation marks

--- schedule/main/models.py
+++ schedule/main/models.py
@@ -2,10 +2,35 @@ from django.db import models
 from django.conf import settings
 
 
+class AvaibleConstraints(models.Model):
+    '''Avaible constraints edited by admin only (superuser)'''
+    name = models.CharField(max_length=50, unique=True)
+    updated = models.DateTimeField(auto_now=True)
+    description = models.TextField() # text that deeply explain the constraint
+
+    class Meta:
+        ordering = ['-updated']
+
+
+class Constraints(models.Model):
+    '''Sets of avaible constraints'''
+    representation = models.CharField(max_length=50)
+    avaible_constraints = models.ManyToManyField(AvaibleConstraints, blank=True) # Many constraints - many avaible constraints
+    updated = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-updated']
+        unique_together = (('representation', 'user'),) # Unique name of the group for a given user
+
+    def __str__(self) -> str:
+        return f'{self.representation}'
+
+
 class Group(models.Model):
     group_name = models.CharField(max_length=50)
     num_of_shifts = models.PositiveSmallIntegerField()
-    constraints = models.ForeignKey('Constraints', on_delete=models.SET_NULL, blank=True, null=True)
+    constraints = models.ForeignKey(Constraints, on_delete=models.SET_NULL, blank=True, null=True)
     # one set of constraints can be assigned to multiple groups
     # SET_NULL - Set this field to null if constraints are deleted
     hide = models.BooleanField(default=False)
@@ -18,7 +43,7 @@ class Group(models.Model):
 
     def __str__(self) -> str:
         return self.group_name
-    
+
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=30)
@@ -49,31 +74,3 @@ class Shift(models.Model):
 
     def __str__(self) -> str:
         return f'{self.employee.first_name}, {self.date}, {self.shift_num}'
-
-
-class AvaibleConstraints(models.Model):
-    '''Avaible constraints edited by admin only (superuser)'''
-    name = models.CharField(max_length=50, unique=True)
-    updated = models.DateTimeField(auto_now=True)
-    description = models.TextField() # text that deeply explain the constraint
-
-    class Meta:
-        ordering = ['-updated']
-
-
-class Constraints(models.Model):
-    '''Sets of avaible constraints'''
-    representation = models.CharField(max_length=50)
-    avaible_constraints = models.ManyToManyField(AvaibleConstraints, blank=True) # Many constraints - many avaible constraints
-    updated = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
-    class Meta:
-        ordering = ['-updated']
-        unique_together = (('representation', 'user'),) # Unique name of the group for a given user
-
-    def __str__(self) -> str:
-        return f'{self.representation}'
-
-
-# user - users can modify only rows created by them
